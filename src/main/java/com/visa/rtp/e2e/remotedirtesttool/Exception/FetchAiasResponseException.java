a44cb9ce258fa80e766e00997abebc492e1e6b2b
athabeti <abhiselec@gmail.com>
2020-07-16 15:26:38 -0700

    Create FetchAiasResponseException.java

---
+++ src/main/java/com/visa/rtp/e2e/remotedirtesttool/Exception/FetchAiasResponseException.java
@@ -0,0 +1,13 @@
+package com.visa.rtp.e2e.remotedirtesttool.Exception;
+
+public class FetchAiasResponseException extends RuntimeException{
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6131825720874042718L;
+
+    public FetchAiasResponseException(String msg) {
+        super(msg);
+    }
+}
