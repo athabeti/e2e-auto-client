8d8ccf10a89d9ad3e35baab5efcb10a8c36e5f23
athabeti <abhiselec@gmail.com>
2020-07-16 15:23:21 -0700

    Create FetApiRequestBodyException.java

---
+++ src/main/java/com/visa/rtp/e2e/remotedirtesttool/Exception/FetApiRequestBodyException.java
@@ -0,0 +1,14 @@
+package com.visa.rtp.e2e.remotedirtesttool.Exception;
+
+public class FetApiRequestBodyException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 6528970680687963694L;
+
+    public FetApiRequestBodyException(String msg) {
+        super(msg);
+    }
+
+}
