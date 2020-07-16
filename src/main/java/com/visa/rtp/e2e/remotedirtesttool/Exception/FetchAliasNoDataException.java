995e8eaba1bb928b66d633a20243f4b60f2f4d82
athabeti <abhiselec@gmail.com>
2020-07-16 15:27:53 -0700

    Create FetchAliasNoDataException.java

---
+++ src/main/java/com/visa/rtp/e2e/remotedirtesttool/Exception/FetchAliasNoDataException.java
@@ -0,0 +1,13 @@
+package com.visa.rtp.e2e.remotedirtesttool.Exception;
+
+public class FetchAliasNoDataException extends RuntimeException {
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 1301617856725784526L;
+
+    public FetchAliasNoDataException(String msg) {
+        super(msg);
+    }
+}
